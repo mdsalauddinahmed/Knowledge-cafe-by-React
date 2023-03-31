0e9094284637f963a2c74382702579cda0ec847c
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-03-31 12:52:30 +0600

    sideCart item done

--- src/components/sideCart/SideCart.jsx
+++ src/components/sideCart/SideCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SideCart = () => {
     return (
         <div>
-            <h1>hello world</h1>
+             <p className='text-center border m-3 fst-normal py-2 text-info rounded  '>Spent time on read : min</p>
         </div>
     );
 };
